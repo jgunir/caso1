2d8bb70795e1828ac2acd5b204c5634fe5a4ed0b
Jesús Galán <jgc.developments@gmail.com>
2021-02-14 19:48:59 +0100

    fix: clean step for docker

---
+++ pipeline/PIPELINE-TEST/2-testing.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source py36-venv/bin/activate
+set -x
+flake8 todos
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+radon cc todos
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+bandit -r todos
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+#ommitted by no module _sqlite3
+coverage run test/unified/TestToDoClass.py
+coverage report -m
