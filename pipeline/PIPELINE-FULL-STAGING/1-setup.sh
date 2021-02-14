3358f8b3a00434943c176c24af4ede3f7dcb647f
Jesús Galán <jgc.developments@gmail.com>
2021-02-14 17:40:49 +0100

    fix: coverage

--- pipeline/PIPELINE-FULL-STAGING/1-setup.sh
+++ pipeline/PIPELINE-FULL-STAGING/1-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
-sudo yum update
+sudo yum update -y
 sudo yum install -y python36 python36-pip
 
+docker stop `docker ps -q`
 
 docker run -d --name dynamodb -p 8000:8000 amazon/dynamodb-local
 sleep 10
