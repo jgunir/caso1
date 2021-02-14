76a73722bea3af7366970ad684acff1243202212
Jesús Galán <jgc.developments@gmail.com>
2021-02-14 16:25:09 +0100

    fix: jenkins pipeline for serverless project

--- pipeline/PIPELINE-FULL-STAGING/1-setup.sh
+++ pipeline/PIPELINE-FULL-STAGING/1-setup.sh
@@ -6,9 +6,4 @@ source py37-venv/bin/activate
 pip install -r todos/requirements.txt
 #python src/createTable.py
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-node -e "console.log('Running Node.js ' + process.version)"
 node --version
-npm install -g serverless
