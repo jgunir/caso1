bcf336d5ff9325213950b5605fd6edec903c4e70
Jesús Galán <jgc.developments@gmail.com>
2021-02-14 16:31:08 +0100

    fix: jenkins pipeline for serverless project

--- pipeline/PIPELINE-FULL-STAGING/1-setup.sh
+++ pipeline/PIPELINE-FULL-STAGING/1-setup.sh
@@ -8,5 +8,5 @@ pip install -r todos/requirements.txt
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install --lts
 npm install -g serverless
