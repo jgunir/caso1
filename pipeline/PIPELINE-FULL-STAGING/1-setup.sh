bebb7bb2e6ab5452720b211db74e988df6f8a388
Jesús Galán <jgc.developments@gmail.com>
2021-02-14 16:48:08 +0100

    fix: setup node in build step

--- pipeline/PIPELINE-FULL-STAGING/1-setup.sh
+++ pipeline/PIPELINE-FULL-STAGING/1-setup.sh
@@ -5,8 +5,3 @@ python3.7 -m venv py37-venv
 source py37-venv/bin/activate
 pip install -r todos/requirements.txt
 #python src/createTable.py
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install --lts
-npm install -g serverless
--- pipeline/PIPELINE-FULL-STAGING/3-build.sh
+++ pipeline/PIPELINE-FULL-STAGING/3-build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+npm install -g serverless
 serverless deploy
