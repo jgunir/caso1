fddc64f84aaa169607f1e0dd0361acd0bb9bb672
Jesús Galán <jgc.developments@gmail.com>
2021-02-14 16:56:16 +0100

    feat: create pull request to deploy

--- pipeline/PIPELINE-FULL-STAGING/3-build.sh
+++ pipeline/PIPELINE-FULL-STAGING/3-build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install --lts
-npm install -g serverless
-serverless deploy
+# Send pull request to activate serverless pipeline
+git request-pull develop ./
